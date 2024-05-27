5fec45a5ca1b9cb60fe869c446a28ca477d69f44
solomon-ojo <woleojosolomon@gmail.com>
2024-05-27 11:35:18 +0200

    all

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -5,22 +5,16 @@ import Logo from '@/public/images/logo.png'
 export default function Footer() {
   return (
     <footer>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
-        {/* Top area: Blocks */}
-        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-zinc-200">
-
-          {/* 1st block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-6 max-sm:order-1 flex flex-col">
-            <div className="mb-4">
+      <div className='flex flex-col items-center md:py-6 border-t border-zinc-200'>
+          <div className="mb-4 flex flex-row items-center gap-4">
               {/* Logo */}
               <Link className="flex items-center justify-center bg-white w-8 h-8 rounded shadow-sm shadow-zinc-950/20" href="/">
                 <Image src={Logo} width={24} height={24} alt="Logo" />
               </Link>
-            </div>
-            <div className="grow text-sm text-zinc-500">&copy; Cruip.com. All rights reserved.</div>
-            {/* Social links */}
-            <ul className="flex space-x-4 mt-4 mb-1">
+              <div className="grow text-sm text-zinc-500">&copy; Bourgeon.co. All rights reserved.</div>
+          </div>
+          <div className=''>
+            <ul className="flex space-x-4 mb-1">
               <li>
                 <a className="flex justify-center items-center text-zinc-700 hover:text-zinc-900 transition" href="#0" aria-label="Twitter">
                   <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
@@ -44,65 +38,6 @@ export default function Footer() {
               </li>
             </ul>
           </div>
-
-          {/* 2nd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-sm text-zinc-800 font-medium mb-2">Company</h6>
-            <ul className="text-sm space-y-2">
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">About us</a>
-              </li>
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Diversity & Inclusion</a>
-              </li>
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Blog</a>
-              </li>
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Careers</a>
-              </li>
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Financial statements</a>
-              </li>
-            </ul>
-          </div>
-
-          {/* 3rd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-sm text-zinc-800 font-medium mb-2">Resources</h6>
-            <ul className="text-sm space-y-2">
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Community</a>
-              </li>
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Terms of service</a>
-              </li>
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Collaboration features</a>
-              </li>
-            </ul>
-          </div>
-
-          {/* 4th block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-sm text-zinc-800 font-medium mb-2">Legals</h6>
-            <ul className="text-sm space-y-2">
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Refund policy</a>
-              </li>
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Terms & Conditions</a>
-              </li>
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Privacy policy</a>
-              </li>
-              <li>
-                <a className="text-zinc-500 hover:text-zinc-900 transition" href="#0">Brand Kit</a>
-              </li>
-            </ul>
-          </div>
-
-        </div>
 
       </div>
     </footer>
