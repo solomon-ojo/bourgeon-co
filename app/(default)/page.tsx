a6b91a70f593401ea9c3ffb4fe7ef58431a286a6
solomon-ojo <woleojosolomon@gmail.com>
2024-05-27 11:47:46 +0200

    all

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -10,12 +10,14 @@ import Features03 from '@/components/features-03'
 import PricingTabs from '@/components/pricing-tabs'
 import Testimonials from '@/components/testimonials'
 import Cta from '@/components/cta'
+import Cta2 from '@/components/cta2'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features01 />
+      <Cta2 />
       <Features03 />
       <Features02 />
 
