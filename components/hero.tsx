0f6098442e583c630975900e3376b7b5ed9b63d0
solomon-ojo <woleojosolomon@gmail.com>
2024-05-27 12:30:21 +0200

    all

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ import Stats from '@/components/stats'
 export default function Hero() {
   return (
     <section className="relative before:absolute before:inset-0 before:h-80 before:pointer-events-none before:bg-gradient-to-b before:from-zinc-100 before:-z-10">
-      <div className="pt-32 pb-6 md:pt-20 md:pb-20">
+      <div className="pt-16 pb-6 md:pt-20 md:pb-20">
         {/* Section content */}
         <div className="px-4 sm:px-6">
           <div className="max-w-3xl mx-auto">
